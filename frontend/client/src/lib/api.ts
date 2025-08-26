a0eeae34af55097bb4635516b947d63b115b4458
ayush26009 <k.ayush@unque.me>
2025-08-26 22:33:25 +0530

    Prod

--- frontend/client/src/lib/api.ts
+++ frontend/client/src/lib/api.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE || "http://localhost:8080/api",
   withCredentials: true, // keep cookies
+  headers: { "Content-Type": "application/json" },
 });
 
 api.interceptors.request.use((config) => {
